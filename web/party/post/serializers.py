3cd51dff4292a9ec0b9494a45a22cf101f2611df
Fray23 <mars6358@gmail.com>
2019-11-22 17:23:53 +0600

    post app added

---
+++ web/party/post/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework.serializers import ModelSerializer
+from party.post.models import Post, PostImages
+
+
+class PostImageSerializer(ModelSerializer):
+    def _get_picture_url(self, obj):
+        try:
+            if obj.picture is None:
+                return None
+
+            url = obj.picture.url
+            request = self.context.get('request')
+            if request is not None:
+                return request.build_absolute_uri(url)
+            return url
+        except:
+            return ''
+
+    def to_representation(self, instance):
+        res = super().to_representation(instance)
+        res['picture'] = self._get_picture_url(instance)
+        return res
+
+    class Meta:
+        model = PostImages
+        fields = ['picture']
+
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['images'] = PostImageSerializer(instance.images.all(), many=True, context=self.context).data
+        return representation
