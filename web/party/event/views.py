7ec36c2c0bec4a3b827e0fab16dfc4d2f2578ebd
Fray23 <mars6358@gmail.com>
2019-11-27 12:06:50 +0600

    permission for event

--- web/party/event/views.py
+++ web/party/event/views.py
@@ -1,16 +1,27 @@
 from rest_framework import mixins, viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from party.core.permissions import IsOwnerOrIsAdmin, IsAdmin
 from party.event.models import Event
 from party.event.serializers import EventSerializer
 
 
 class EventViewSet(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  mixins.UpdateModelMixin,
-                  viewsets.GenericViewSet):
+                   mixins.CreateModelMixin,
+                   mixins.UpdateModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy', 'create']:
+            permission_classes = [IsOwnerOrIsAdmin]
+        elif self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdmin]
+        return [permission() for permission in permission_classes]
 
     def get_queryset(self):
         month = self.request.GET.get('month', None)
