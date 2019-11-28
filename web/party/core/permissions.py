73cf0ba7baf91434893653a19702ad39f4938d41
Fray23 <mars6358@gmail.com>
2019-11-28 11:18:22 +0600

    custom paginatar added

--- web/party/core/permissions.py
+++ web/party/core/permissions.py
@@ -25,14 +25,10 @@ class IsPostImageOwnerOrAdmin(permissions.BasePermission):
 
 
 class IsUserOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
+    def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS or
             request.user and
-            request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return bool(
+            request.user.is_authenticated and
             obj == request.user
         )
