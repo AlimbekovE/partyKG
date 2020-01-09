ac58519c2ca84b076ab994578a1e7aae559e9ece
fray23 <mars6358@gmail.com>
2020-01-10 04:19:50 +0600

    vote app added

--- web/party/urls.py
+++ web/party/urls.py
@@ -6,7 +6,8 @@ from rest_framework.routers import DefaultRouter
 
 from party.account.views import qr_code_scan, user_detail, event_users_list, UserView
 from party.event.views import EventViewSet
-from party.post.views import PostViewSet, PostImagesViewsSet, PostCommentViewSet, post_favorite
+from party.post.views import PostViewSet, PostImagesViewsSet, PostCommentViewSet
+from party.vote.views import QuestionViewSet, vote
 
 router = DefaultRouter()
 router.register('post', PostViewSet)
@@ -14,11 +15,13 @@ router.register('post_comments', PostCommentViewSet)
 router.register('post_images', PostImagesViewsSet)
 router.register('users', UserView)
 router.register('event', EventViewSet)
+router.register('question', QuestionViewSet)
 
 urlpatterns = [
     path('qr_code/<int:pk>/', qr_code_scan, name='qr_code'),
     path('event/<int:pk>/users/list/', event_users_list, name='event_visits_list'),
     path('user/detail/<int:pk>/', user_detail, name='user_detail'),
+    path('api/v1/vote', vote),
     path('admin/', admin.site.urls),
     path('api/v1/', include('party.api_auth.urls')),
     path('api/v1/', include(router.urls)),
---
+++ web/party/vote/utils.py
@@ -0,0 +1,7 @@
+from django.utils.translation import gettext as _
+
+ANSWER = (
+    ('behind', _('Behind')),
+    ('against', _('Against')),
+    ('abstain', _('Abstain')),
+)
